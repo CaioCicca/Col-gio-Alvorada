c48445a6225cc301ea7152199c33df31f27d7243
Caio Ciccarelli <caio.ciccarelli@aluno.senai.br>
2024-03-18 11:56:57 -0300

    start models

---
+++ src/Models/Escola.js
@@ -0,0 +1,18 @@
+import { v4 as uuidv4, v5 as uuidv5 } from 'uuid';
+
+class Escola {
+    constructor(nome, corPrimaria, corSecundaria, quantidadeFuncionarios, capacidadeAlunosPorAno, quantidadeTurmas, endereco, telefone, email, responsavel) {
+        this.id = uuidv4()
+        this.nome = nome;
+        this.date =  new Date(2024, 2, 18);
+        this.corPrimaria = corPrimaria;
+        this.corSecundaria = corSecundaria;
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+        this.capacidadeAlunosPorAno = capacidadeAlunosPorAno;
+        this.quantidadeTurmas = quantidadeTurmas;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.email = email;
+        this.responsavel = responsavel;
+    }
+}
---
+++ src/data/Escolas.js
@@ -0,0 +1,17 @@
+export const escolas = {
+  nome:'Colégio Alvorada - Curitiba',
+  corPrimaria:'#274696',
+  corSecundaria:'#3B6DDB',
+  quantidadeFuncionarios: 200,
+  capacidadeAlunosPorAno: 30,
+  quantidadeTurmas: 48,
+  endereco:{
+    bairro: 'Bairro do Sol',
+    numero: 123,
+    cidade: 'Curitiba',
+    estado: 'PR',
+  },
+  telefone: 44932568886,
+  email:'alvorada@gmail.com',
+  responsavel:'Alvaro Souza'
+};
